47f3405ef3a7aa749154ac640f7d43f78d851dc2
ultramenid <malichamdan@gmail.com>
2021-10-04 20:40:48 +0700

    add forceHTTPS

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//https force https
+URL::forceScheme('https');
 
 Route::get('/', function () {
     return view('frontend.index');
