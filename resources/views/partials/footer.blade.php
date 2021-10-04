9fe0a3041005a5d48f5d9cd4e285be42ebf2bd68
ultramenid <malichamdan@gmail.com>
2021-10-04 21:47:07 +0700

    make it better

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,12 +6,12 @@
         </div>
 
         <div class="flex sm:flex-row flex-col justify-between sm:space-x-10 space-x-0 space sm:space-y-0 space-y-6">
-            <p class="sm:w-6/12 w-full text-white text-sm">AboutVolo est, tet voluptament aliqui dentin re plitis pa quaspictem quossit, andiat. Pa parum quis sin nonsequi tor as alis et, similique nem qui nobit ipsumque ad milluptae pa sitae labore, ne vid ent id quam sunt.Dusaes mi, sitatiust hicidia veria quodi inus pelibeatium expersperum nulluptatur? Quis aut ut venihilignia quiam, sum laut pe cum quamet quatur, cum faciet lam, volo maiore omnimi, quid molorepratem dolupta sitios nulpa doluptatium volorum veleseq uissum fugia ex et aut qui opta que</p>
-            <div class="sm:w-6/12 w-full">
+            <p class="sm:w-7/12 w-full text-white text-sm">AboutVolo est, tet voluptament aliqui dentin re plitis pa quaspictem quossit, andiat. Pa parum quis sin nonsequi tor as alis et, similique nem qui nobit ipsumque ad milluptae pa sitae labore, ne vid ent id quam sunt.Dusaes mi, sitatiust hicidia veria quodi inus pelibeatium expersperum nulluptatur? Quis aut ut venihilignia quiam, sum laut pe cum quamet quatur, cum faciet lam, volo maiore omnimi, quid molorepratem dolupta sitios nulpa doluptatium volorum veleseq uissum fugia ex et aut qui opta que</p>
+            <div class="sm:w-5/12 w-full">
                 <p class="font-semibold text-white text-xl">Stay informed with NDPE updates</p>
-                <div class="flex justify-between">
+                <div class="flex justify-between mt-2">
                     <input type="text" class="w-9/12 py-2 px-2 focus:outline-none bg-brown-ndpe" placeholder="enter your email here">
-                    <button class="text-sm text-wrap w-3/12 bg-green-ndpe" style="color: #277a28">SIGN ME UP</button>
+                    <button class="sm:text-sm  text-xs text-wrap w-3/12 bg-green-ndpe" style="color: #277a28">SIGN ME UP</button>
                 </div>
                 <p class="text-xss text-white mt-4 ">You can unsubscribe at any time by clicking the link in the footer of our emails. We use Mailchimp as our marketing platform. By subscribing, you acknowledge that your information will be transferred to Mailchimp for processing. Learn more about Mailchimp’s privacy practices here.</p>
             </div>
