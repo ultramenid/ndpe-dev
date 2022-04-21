746fa9cdb6f7e8301775bbd27652cfd568412785
ultramenid <malichamdan@gmail.com>
2022-04-21 16:00:58 +0700

    fix indonesia corporate

--- app/Http/Controllers/CorporatesController.php
+++ app/Http/Controllers/CorporatesController.php
@@ -65,7 +65,7 @@ class CorporatesController extends Controller
     }
     public function getSelectCorporate(){
         if (App::getLocale() == 'id') {
-            return 'name, descID as description,overviewID as overview, areaID as area, ownershipID as ownership, financialID as financial, buyerID as buyer, performanceID as performance, nilai1, nilai2, img, nilai1DescdID as nilai1Desc , nilai2DescID as nilai2Desc';
+            return 'name, descID as description,overviewID as overview, areaID as area, ownershipID as ownership, financialID as financial, buyerID as buyer, performanceID as performance, nilai1, nilai2, img, nilai1DescID as nilai1Desc , nilai2DescID as nilai2Desc';
         }elseif(App::getLocale() == 'jp'){
             return 'name, descJP as description,overviewJP as overview, areaJP as area, ownershipJP as ownership, financialJP as financial, buyerJP as buyer, performanceJP as performance, nilai1, nilai2, img, nilai1DescJP as nilai1Desc, nilai2DescJP as nilai2Desc';
         }else{
