d53c5f09e6109f0d159e1d16f26e34875030110a
ultramenid <malichamdan@gmail.com>
2022-08-04 13:34:16 +0700

    height of hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,14 +1,15 @@
 <section class="relative">
-    <div class="min-h-screen bg-update-ndpe py-6" style="
+    <div class=" bg-update-ndpe py-6" style="
         background-image: url({{ asset('assets/new-bg.jpg') }});
         background-repeat: no-repat;
         background-size: cover;
+        height: 80vh;
         background-color: transparent;
         background-blend-mode:multiply;">
             @include('partials.nav')
 
         <!-- Span bottom edge -->
-        <div class="relative z-10 max-w-6xl mx-auto" style="height: 70vh">
+        <div class="relative z-10 max-w-6xl mx-auto" style="height: 50vh">
             <div class="absolute inset-x-0 sm:bottom-14 bottom-28 h-16 px-8">
                 <p class="text-brown-ndpe sm:text-6xl text-4xl font-notoserif">
                     {{__('Saving the remaining natural forest cover')}}
